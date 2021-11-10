f8b95a4348453c2b5abed23e317101a6416a8639
Sergjio97 <sergiocizmja@gmail.com>
2021-11-10 15:28:27 +0100

    console.log mail

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
-const utentiRegistrati = ["abc@gmail.com", "dfg@gmail.com", "hil@gmail.com"];
-console.log(utentiRegistrati);
+const login = prompt("inserisci la tua mail");
+
+const utentiRegistrati = ["abc@gmail.com","dfg@gmail.com","hil@gmail.com"];
+
+let sentinella = false;
+
+for (let i = 0; i < utentiRegistrati.length && sentinella == false; i++){
+
+    if (utentiRegistrati[i] == login){
+        sentinella = true;
+    }
+}
+
+if (sentinella == true){
+    console.log("ok");
+} else {
+    console.log("no");
+}
